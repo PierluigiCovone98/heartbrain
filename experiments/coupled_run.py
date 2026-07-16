0993cfccb319f5bfcc25d7de7055e8eb2664ce8b
PierluigiCovone98 <covone.1895077@studenti.uniroma1.it>
2026-07-16 17:54:16 +0200

    Update the "coupled_run" experiment such that "k_hb=0.3"

--- experiments/coupled_run.py
+++ experiments/coupled_run.py
@@ -27,7 +27,7 @@ SIGMA = 0.0
 
 # === coupled
 N_STEPS = 10000
-K_HB = 0.0
+K_HB = 0.3
 
 # === brain signal filtering
 CUTOFF_PERIOD = 100
@@ -97,17 +97,18 @@ def main():
         h.step(sigma=SIGMA, dt=DT)
 
     # Plotting1
-    plotting.plot_time_series(heart_time_series, brain_time_series, name="chaotic_coupled_run_10K")
+    # plotting.plot_time_series(heart_time_series, brain_time_series, name="chaotic_coupled_run_10K")
     # plotting.plot_single_series(brain_time_series, "chaotic_coupled_run_10K_brain_zoom1",start=2000, end=2601)
     # plotting.plot_single_series(brain_time_series, "chaotic_coupled_run_10K_brain_zoom2",start=2000, end=2101)
+    
     # plotting.plot_time_series(heart_time_series, brain_time_series, name="chaotic_coupled_run_10K_win", start=2000, end=3200)
-
+    plotting.plot_time_series(heart_time_series, brain_time_series, name="chaotic_coupled_run_10K_khb_03", start=2000, end=3200)
 
     # === Brain time series filtering ====
     # brain_slow = analysis.low_pass_filter(signal=brain_time_series, cutoff_period=CUTOFF_PERIOD)
 
     # Plotting2
-    # plotting.plot_time_series(heart_time_series, brain_slow, name="coupled_run_10K_khb_05_filtered1", start=2000, end=3200, brain_label="brain  s_slow(t)")
+    # plotting.plot_time_series(heart_time_series, brain_slow, name="chaotic_coupled_run_10K_filtered1", start=2000, end=3200, brain_label="brain  s_slow(t)")
 
 
     # # === Phase extraction ===
